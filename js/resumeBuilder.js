092035cd5f67bd192a9f0e95ca2adc0fbb8d8d29
xian0831 <xian0831@gmail.com>
2015-08-06 23:33:53 -0400

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%","Andrew")
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
