deee23b093798db5a30dec13b13e6baabb856c80
xian0831 <xian0831@gmail.com>
2015-08-07 06:49:17 -0400

    Add bio information

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,33 @@ var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var bio = {
+	"name" : "Andrew Zheng",
+	"role" : "Front End Developer",
+	"contacts" : {
+		"mobile" : "631-741-2474",
+		"email" : "xian0831@gmail.com"
+	},
+	"bioPic" : "images/fry.jpg",
+	"WelcomeMessage" : "welcome to my home page",
+	"skills" : [
+		"java","HTML5","JavaScript"
+	]
+}
+
+var formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.WelcomeMessage);
+var formattedSkills = HTMLskills.replace("%data%",bio.skills);
+var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+$("#topContacts").append(formattedBioPic);
+$("#topContacts").append(formattedWelcomeMsg);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedSkills);
+
+
+
+
+
