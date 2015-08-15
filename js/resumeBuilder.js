241e845fa37528ad0d45632f90a7eb5db7021dee
xian0831 <xian0831@gmail.com>
2015-08-15 17:43:44 -0400

    Add resume content

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,31 +9,116 @@ $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
 var bio = {
-	"name" : "Andrew Zheng",
-	"role" : "Front End Developer",
-	"contacts" : {
-		"mobile" : "631-741-2474",
-		"email" : "xian0831@gmail.com"
+	"name": "Andrew Zheng",
+	"role": "Front End Developer",
+	"contacts": {
+		"mobile": "631-741-2474",
+		"email": "xian0831@gmail.com",
+		"github": "xian0831"
 	},
-	"bioPic" : "images/fry.jpg",
-	"WelcomeMessage" : "welcome to my home page",
-	"skills" : [
+	"bioPic": "images/fry.jpg",
+	"WelcomeMessage": "welcome to my home page",
+	"skills": [
 		"java","HTML5","JavaScript"
 	]
 }
 
+var work = {
+	"jobs": [
+		{
+			"employer": "Prudential Financial",
+			"title": "System Development Analyst",
+			"location": "Newark NJ",
+			"dates": "08/2014 - Current",
+			"description": "Develop program."
+		},
+		{
+			"employer": "Citigroup",
+			"title": "Technology Analyst",
+			"location": "New York NY",
+			"dates": "06/2013 - 06/2014",
+			"description": "Develop program."
+		}
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name": "Syracuse University",
+			"city": "Syracuse, NY",
+			"degree": "Masters",
+			"major": "CompSci",
+			"graduation": "May 2017",
+			"url": "http://www.syr.edu/"
+		},
+		{
+			"name": "Stony Brook University",
+			"city": "Stony Brook, NY",
+			"degree": "BS",
+			"major": "CompSci",
+			"graduation": "Dec 2013",
+			"url": "http://www.stonybrook.edu/"
+		}
+	],
+	"onlineCourses": [
+        {
+            "title": "Intro to JavaScript",
+            "school": "Udacity",
+            "dates": "07/2015",
+            "url": "https://www.udacity.com/"
+        }
+
+    ]
+
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Lunch Buddy",
+			"dates": "01/2015 - 05/2015",
+			"description": "school project"
+		}
+	]
+}
+
+
 var formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPic);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.WelcomeMessage);
-var formattedSkills = HTMLskills.replace("%data%",bio.skills);
-var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
-var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
-$("#topContacts").append(formattedBioPic);
-$("#topContacts").append(formattedWelcomeMsg);
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedSkills);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
 
+if (bio.hasOwnProperty("skills") || bio.skills.length>0){
 
+    $("#header").append(HTMLskillsStart);
+    var index;
+    for(index=0;index<bio.skills.length;index++){
+        var formattedSkills = HTMLskills.replace("%data%",bio.skills[index]);
+        $("#skills").append(formattedSkills);
+    }
 
+}
 
+if (work.hasOwnProperty("jobs") || bio.skills.length>0){
 
+    displayWork();
+}
+
+function displayWork() {
+    for(job in work.jobs){
+        //create a new div for work experience entry
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedWorkDate = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+        $(".work-entry:last").append(formattedWorkDate);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+}
